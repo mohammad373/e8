a254e9a71d92da228f2ace18d0d78835af865028
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-14 14:03:51 +0330

    Update and rename README.md to e8.py

---
+++ e8.py
@@ -0,0 +1,65 @@
+# 
+
+
+import os
+import time
+import requests
+import socket
+import sys
+import datetime
+from colorama import Fore
+
+def __target1__():
+    os.system("clear")
+    time.sleep(0.5)
+    print(Fore.RED +"""
+        ///////////////////////
+        //////////////////////
+        /////////////////////
+        ////////////////////
+        ///////////////////
+        //////////////////
+        /////////////////
+        ////////////////
+        ///////////////
+        //////////////
+        /////////////
+        ////////////
+        ///////////
+        //////////
+        /////////
+        ////////
+        ///////
+        //////
+        /////
+        ////
+        ///
+        //
+        /""")
+    target = input(Fore.BLUE + "\n[!]" + Fore.RED + " ~ " + Fore.YELLOW + "Pleass Enter Your Address Target" + Fore.GREEN + " ==>  ")
+    if target == "" or None:
+        try:
+            time.sleep(0.5)
+            print(Fore.RED + "\nError : Your Target Is None Or Not Found ;(")
+            time.sleep(1)
+            sys.exit()
+        except:
+            pass
+    if not "http" in target or not "https" in target :
+        target = "http://" + target
+    # ip = socket.gethostbyname(target)
+    r = requests.get(target)
+    time = datetime.datetime.now()
+    while True:
+         coun = 1   
+         if r.status_code == 404 or r.status_code == 500:
+             print(Fore.YELLOW + "[ " + Fore.RED + coun+ Fore.YELLOW + " ]" + Fore.BLUE + " ~ " + Fore.RED + target + Fore.BLUE  + "  |  " + Fore.RED + time)
+             coun += 1
+         if r.status_code == 200:
+             print(Fore.BLUE + coun + Fore.BLUE + " ~ " + Fore.GREEN + target + Fore.YELLOW + "  |  " + Fore.GREEN + time)
+             coun += 1
+         if coun == 10000:
+            time.sleep(0.1)
+            sys.exit()
+
+__target1__()
